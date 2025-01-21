8ad93879e38fa0b5b4baf51a1d358fff1fc66851
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-21 18:54:00 +0300

    Create init.sql

---
+++ base/init.sql
@@ -0,0 +1,9 @@
+-- Drop existing tables
+DROP TABLE IF EXISTS hetra;
+DROP TABLE IF EXISTS paiement_impots;
+DROP TABLE IF EXISTS arrondissement_position;
+DROP TABLE IF EXISTS arrondissement;
+DROP TABLE IF EXISTS maison_detaills;
+DROP TABLE IF EXISTS maison;
+DROP TABLE IF EXISTS type_rindrina;
+DROP TABLE IF EXISTS type_tafo;
